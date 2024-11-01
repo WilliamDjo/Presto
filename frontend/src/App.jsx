48195cb670150cc280879ef8aee64380192043a6
Spandan Shrestha <spandan@Spandans-MacBook-Pro.local>
2024-11-01 18:14:05 +1100

    pulling from master

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,24 @@
-import { useState } from 'react';
-import SignUpPage from './Pages/SignUpPage/SignUp';
-import SignInPage from './Pages/SignInPage/SignIn';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+import Login from './Pages/SignIn/Login'; // Assume you have a SignIn component
 import LandingPage from './Pages/LandingPage/LandingPage';
-import DashboardPage from './Pages/SignInPage/DashboardPage/Dashboard';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignUp from './Pages/SignUp/SignUp';
+
 
 
 function App() {
-  const [page, setPage] = useState('SignUp');
-
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path="/register" element={<SignUpPage />} />
-        <Route path="/login" element={<SignInPage />} />
         <Route path="/" element={<LandingPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/register" element={<Register />} /> */}
+        <Route path="*" element={<Navigate to="/" />} /> {/* Redirect unknown routes to landing page */}
+        <Route path="/register" element={<SignUp />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
