2b6fde89616a29c674512ce634b5a32d281fd4a6
Spandan Shrestha <spandan@Spandans-MacBook-Pro.local>
2024-11-04 18:41:06 +1100

    Added CentralPanel and CustomPaper to have universal consistent style for those elements

---
+++ frontend/src/Components/CentralPanel.jsx
@@ -0,0 +1,21 @@
+import { Container, Box } from '@mui/material';
+import CustomPaper from './CustomPaper';
+
+
+const CentralPanel = ({ children, maxWidth = "xs" }) => {
+    return (
+        <Box display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="100vh"
+        >
+            <Container maxWidth={maxWidth}>
+                <CustomPaper>
+                    {children}
+                </CustomPaper>
+            </Container>
+        </Box>
+    );
+};
+
+export default CentralPanel;
--- frontend/src/Pages/LandingPage/Landing.jsx
+++ frontend/src/Pages/LandingPage/Landing.jsx
@@ -1,42 +1,41 @@
 import { Container, Typography, Box, Paper } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import CustomButton from '../../components/CustomButton';
+import CentralPanel from '../../Components/CentralPanel';
 
 const LandingPage = () => {
   const navigate = useNavigate();
 
   return (
-    <Container maxWidth="md" sx={{ mt: 8 }}>
-      <Paper elevation={3} sx={{ p: 4, borderRadius: 2, textAlign: 'center' }}>
-        <Typography variant="h3" component="h1" fontWeight="bold" gutterBottom>
-          Presto
-        </Typography>
+    <CentralPanel maxWidth="md" sx={{ mt: 8 }}>
+      <Typography variant="h3" component="h1" fontWeight="bold" gutterBottom>
+        Presto
+      </Typography>
 
-        <Typography variant="h6" color="textSecondary" sx={{ mb: 4 }}>
-          A lean, lightweight app that is a lot more enjoyable and interesting to use and that will revolutionise the presentations industry for decades to come!
-        </Typography>
-
+      <Typography variant="h6" color="textSecondary" sx={{ mb: 4 }}>
+        A lean, lightweight app that is a lot more enjoyable and interesting to use and that will revolutionise the presentations industry for decades to come!
+      </Typography>
 
 
-        <Box display="flex" justifyContent="center" gap={2} mt={2}>
-          <CustomButton
-            text="Login"
-            variant='contained'
-            onClick={() => navigate('/login')}
-            fullWidth={false}
-            sx={{ py: 1.5, px: 4 }}
-          />
 
-          <CustomButton
-            text="Register"
-            variant='outlined'
-            onClick={() => navigate('/register')}
-            fullWidth={false}
-            sx={{ py: 1.5, px: 4 }}
-          />
-        </Box>
-      </Paper>
-    </Container>
+      <Box display="flex" justifyContent="center" gap={2} mt={2}>
+        <CustomButton
+          text="Login"
+          variant='contained'
+          onClick={() => navigate('/login')}
+          fullWidth={false}
+          sx={{ py: 1.5, px: 4 }}
+        />
+
+        <CustomButton
+          text="Register"
+          variant='outlined'
+          onClick={() => navigate('/register')}
+          fullWidth={false}
+          sx={{ py: 1.5, px: 4 }}
+        />
+      </Box>
+    </CentralPanel>
   );
 };
 
