72fa1e62b069dce7b904453ece7e6b14ddd2af29
Spandan Shrestha <spandan@Spandans-MacBook-Pro.local>
2024-11-04 15:53:26 +1100

    adding back cus button

---
+++ frontend/src/Components/CustomButton.jsx
@@ -0,0 +1,21 @@
+// CustomButton.js
+import { Button } from '@mui/material';
+
+const CustomButton = ({ text, onClick, variant = 'contained', color = 'primary', size = 'large', fullWidth = true, sx = {}, type = 'submit', startIcon = null }) => {
+    return (
+        <Button
+            variant={variant}
+            color={color}
+            size={size}
+            onClick={onClick}
+            fullWidth={fullWidth}
+            sx={{ ...sx }}
+            type={type}
+            startIcon={startIcon}
+        >
+            {text}
+        </Button>
+    );
+};
+
+export default CustomButton;
