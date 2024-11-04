1f40757ef72e7c365c499cf540f3919d7a97da7d
Spandan Shrestha <spandan@Spandans-MacBook-Pro.local>
2024-11-04 15:34:45 +1100

    Fixed bug where user could access any route path even if they weren't authenticated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,15 @@
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 import Login from './Pages/LoginPage/Login';
 import LandingPage from './Pages/LandingPage/Landing';
 import SignUp from './Pages/RegisterPage/Register';
 import DashboardPage from './Pages/DashboardPage/Dashboard';
+import Router from './Components/Router';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Navigate to="/" />} />
-        <Route path="/register" element={<SignUp />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-      </Routes>
+      <Router />
     </BrowserRouter>
   );
 }
