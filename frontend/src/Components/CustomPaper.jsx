b1517cc4aa0e6a38a70e4e40e03de74e12857043
Spandan Shrestha <spandan@Spandans-MacBook-Pro.local>
2024-11-05 11:17:47 +1100

    Fixing lint

--- frontend/src/Components/CustomPaper.jsx
+++ frontend/src/Components/CustomPaper.jsx
@@ -2,11 +2,11 @@ import { Paper } from '@mui/material';
 
 
 const CustomPaper = ({ children }) => {
-    return (
-        <Paper elevation={3} sx={{ p: 4, borderRadius: 2 }}>
-            {children}
-        </Paper>
-    );
+  return (
+    <Paper elevation={3} sx={{ p: 4, borderRadius: 2 }}>
+      {children}
+    </Paper>
+  );
 };
 
 export default CustomPaper;
