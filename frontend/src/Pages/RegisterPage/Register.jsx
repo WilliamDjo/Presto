d1ae7e3bc5d2f8bd104455df385b591f174ae42c
Spandan Shrestha <spandan@Spandans-MacBook-Pro.local>
2024-11-05 11:18:31 +1100

    Fixing lint

--- frontend/src/Pages/RegisterPage/Register.jsx
+++ frontend/src/Pages/RegisterPage/Register.jsx
@@ -1,92 +1,91 @@
-import { CssBaseline, ThemeProvider, Box, Paper, Button, TextField, Typography, Link, Alert, Container } from '@mui/material';
+import { CssBaseline, Box, TextField, Typography, Alert } from '@mui/material';
 import CustomLink from '../../Components/CustomLink'
 import CustomButton from '../../Components/CustomButton';
 import CentralPanel from '../../Components/CentralPanel';
-import theme from '../../Themes/themes';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { authFetch } from '../../HelperFiles/helper';
 
 
 const SignUpPage = () => {
-    const [email, setEmail] = useState('');
-    const [name, setName] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
 
-    const handleRegisterClick = async (e) => {
-        e.preventDefault();
-        try {
-            const res = await authFetch({ email, name, password }, '/admin/auth/register');
+  const handleRegisterClick = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await authFetch({ email, name, password }, '/admin/auth/register');
 
-            if (res.success) {
-                localStorage.setItem('token', res.data.token);
-                navigate('/dashboard');
-            } else {
-                setError(res.error);
-            }
-        } catch (error) {
-            setError(error);
-        }
-    };
-    return (
-        <>
-            <CssBaseline />
-            <CentralPanel
-                maxWidth="xs"
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                minHeight="100vh"
-            >
-                <Box display="flex" flexDirection="column" gap={2} component="form" onSubmit={handleRegisterClick}>
-                    <Typography variant="h5" align="center">
+      if (res.success) {
+        localStorage.setItem('token', res.data.token);
+        navigate('/dashboard');
+      } else {
+        setError(res.error);
+      }
+    } catch (error) {
+      setError(error);
+    }
+  };
+  return (
+    <>
+      <CssBaseline />
+      <CentralPanel
+        maxWidth="xs"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+      >
+        <Box display="flex" flexDirection="column" gap={2} component="form" onSubmit={handleRegisterClick}>
+          <Typography variant="h5" align="center">
                         Sign Up
-                    </Typography>
-                    {error && <Alert severity="error" sx={{ mb: 2 }}>{error}</Alert>}
-                    <TextField
-                        label="Full Name"
-                        fullWidth
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                    <TextField
-                        label="Email"
-                        fullWidth
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <TextField
-                        label="Password"
-                        type="password"
-                        fullWidth
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
+          </Typography>
+          {error && <Alert severity="error" sx={{ mb: 2 }}>{error}</Alert>}
+          <TextField
+            label="Full Name"
+            fullWidth
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            label="Email"
+            fullWidth
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            label="Password"
+            type="password"
+            fullWidth
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-                    <CustomButton
-                        text="Register"
-                        onClick={handleRegisterClick}
-                        sx={{ mt: 2 }}
-                    />
+          <CustomButton
+            text="Register"
+            onClick={handleRegisterClick}
+            sx={{ mt: 2 }}
+          />
 
-                </Box>
-                <CustomButton
-                    text="Back to Home"
-                    onClick={() => navigate('/')}
-                    variant="outlined"
-                    sx={{ mt: 2 }}
-                    type="button"
-                />
+        </Box>
+        <CustomButton
+          text="Back to Home"
+          onClick={() => navigate('/')}
+          variant="outlined"
+          sx={{ mt: 2 }}
+          type="button"
+        />
 
-                <Typography variant="body2" align="center" sx={{ mt: 2 }}>
+        <Typography variant="body2" align="center" sx={{ mt: 2 }}>
                     Already have an account?
-                    <CustomLink text="Sign in" navigateTo="/login" />
-                </Typography>
-            </CentralPanel >
-        </>
-    );
+          <CustomLink text="Sign in" navigateTo="/login" />
+        </Typography>
+      </CentralPanel >
+    </>
+  );
 }
 
 export default SignUpPage;
