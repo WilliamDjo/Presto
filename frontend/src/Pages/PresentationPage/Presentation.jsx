758a4f7717f551b76b83fa30fdda684d03fbe4c4
WilliamDjo <william.djong@yahoo.com>
2024-11-06 22:51:06 +1100

    Modularized presentation page by making separate components for delete, edit and header

--- frontend/src/Pages/PresentationPage/Presentation.jsx
+++ frontend/src/Pages/PresentationPage/Presentation.jsx
@@ -62,96 +62,21 @@ const PresentationPage = () => {
         <SlidesBar />
       </Box>
 
-      {/* Delete Modal */}
-      <Dialog
+      <DeleteDialog 
         open={showDeleteDialog}
         onClose={() => setShowDeleteDialog(false)}
-        aria-labelledby="delete-dialog-title"
-        aria-describedby="delete-dialog-description"
-      >
-        <DialogTitle id="delete-dialog-title">
-          Delete Presentation
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="delete-dialog-description">
-            Are you sure?
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={() => setShowDeleteDialog(false)} color="primary">
-            No
-          </Button>
-          <Button onClick={handleDelete} color="error" autoFocus>
-            Yes
-          </Button>
-        </DialogActions>
-      </Dialog>
+        onConfirm={handleDelete}
+      />
 
-      {/* Settings Modal */}
-      <Dialog 
-        open={showSettingsDialog} 
-        onClose={handleDialogClose}
-        maxWidth="sm"
-        fullWidth
-      >
-        <DialogTitle>Presentation Settings</DialogTitle>
-        <DialogContent>
-          <Stack spacing={3} sx={{ mt: 1 }}>
-            <TextField
-              autoFocus
-              label="Title"
-              fullWidth
-              value={newTitle}
-              onChange={(e) => setNewTitle(e.target.value)}
-              variant="outlined"
-            />
-            
-            <Divider />
-            
-            <Stack spacing={1}>
-              <InputLabel>Thumbnail</InputLabel>
-              <Input
-                type="file"
-                accept="image/*"
-                onChange={handleThumbnailChange}
-                style={{ display: 'none' }}
-                id="thumbnail-upload"
-              />
-              <label htmlFor="thumbnail-upload">
-                <Card sx={{ width: '100%', maxWidth: 345 }}>
-                  <CardActionArea>
-                    <CardMedia
-                      component={previewThumbnail ? "img" : "div"}
-                      sx={{
-                        height: 194,
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        bgcolor: 'grey.100'
-                      }}
-                      image={previewThumbnail}
-                      title="Presentation thumbnail"
-                    >
-                      {!previewThumbnail && (
-                        <Stack alignItems="center" spacing={1}>
-                          <Image color="action" sx={{ fontSize: 40 }} />
-                          <Typography variant="body2" color="text.secondary">
-                            Click to add thumbnail
-                          </Typography>
-                        </Stack>
-                      )}
-                    </CardMedia>
-                  </CardActionArea>
-                </Card>
-              </label>
-            </Stack>
-          </Stack>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleDialogClose}>Cancel</Button>
-          <Button onClick={handleSave} variant="contained">Save</Button>
-        </DialogActions>
-      </Dialog>
+      <SettingsDialog 
+        open={showSettingsDialog}
+        onClose={() => setShowSettingsDialog(false)}
+        onSave={handleSave}
+        title={newTitle}
+        thumbnail={previewThumbnail}
+        onTitleChange={setNewTitle}
+        onThumbnailChange={handleThumbnailChange}
+      />
     </>
   
   );
