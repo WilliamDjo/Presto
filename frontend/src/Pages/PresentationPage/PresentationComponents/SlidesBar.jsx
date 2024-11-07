594161ef01609f70686b659c07669ea91d43e6d2
Spandan Shrestha <spandan@Spandans-MacBook-Pro.local>
2024-11-08 10:27:30 +1100

    Added slides navigation functionality

--- frontend/src/Pages/PresentationPage/PresentationComponents/SlidesBar.jsx
+++ frontend/src/Pages/PresentationPage/PresentationComponents/SlidesBar.jsx
@@ -4,25 +4,30 @@ import SlidesBarComponent from "./SlidePanelsBar/SlidesBarComponent";
 import AddSlidesBarComponent from "./SlidePanelsBar/AddSlidesBarComponent";
 import { useSelector } from 'react-redux';
 import { getSlides } from "../../../HelperFiles/helper";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const SlidesBar = () => {
-  // slides = [
-  //   {
-  //     slideNum: 1
-  //   },
-  //   {
-  //     slideNum: 2
-  //   },
-  //   {
-  //     slideNum: 3
-  //   }
-  // ]
-
   const presentations = useSelector((state) => state.presentations.presentations);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleKeyboardInput = (e) => {
+      if (e.key === 'ArrowLeft' && parseInt(location.hash.split("/")[1]) > 1) {
+        navigate(`${location.pathname}#/${parseInt(location.hash.split("/")[1]) - 1}`)
+      }
+      if (e.key === 'ArrowRight' && parseInt(location.hash.split("/")[1]) < getSlides(presentations).length) {
+        navigate(`${location.pathname}#/${parseInt(location.hash.split("/")[1]) + 1}`)
+      }
+    }
+
+    document.addEventListener("keydown", handleKeyboardInput);
+    return () => document.removeEventListener("keydown", handleKeyboardInput);
+  }, [presentations, navigate]);
 
   return (
     <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", backgroundColor: "primary.main", width: "100%", minHeight: 150 }}>
-      <IconButton disabled={location.hash.split("/")[1] == 1} sx={{ color: 'white' }}>
+      <IconButton disabled={location.hash.split("/")[1] == 1} onClick={() => {navigate(`${location.pathname}#/${parseInt(location.hash.split("/")[1]) - 1}`)}} sx={{ color: 'white' }}>
         <ArrowLeft />
       </IconButton >
       <Box sx={{display: "flex", alignItems: "center", gap: "2%", width: "100%", height: "100%", overflowX: "auto"}}>
@@ -32,7 +37,7 @@ const SlidesBar = () => {
         
         <AddSlidesBarComponent />
       </Box>
-      <IconButton disabled={location.hash.split("/")[1] == getSlides(presentations)?.length} sx={{ color: 'white' }}>
+      <IconButton disabled={location.hash.split("/")[1] == getSlides(presentations)?.length} onClick={() => {navigate(`${location.pathname}#/${parseInt(location.hash.split("/")[1]) + 1}`)}} sx={{ color: 'white' }}>
         <ArrowRight />
       </IconButton>
     </Box>
