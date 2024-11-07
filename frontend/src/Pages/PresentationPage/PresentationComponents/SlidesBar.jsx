5fe996225dc1ba8a2ff2ad2a15358a7a921e5ee5
Spandan Shrestha <spandan@Spandans-MacBook-Pro.local>
2024-11-07 20:02:37 +1100

    Turned slides bar into a component and added some funcitonality to it

--- frontend/src/Pages/PresentationPage/PresentationComponents/SlidesBar.jsx
+++ frontend/src/Pages/PresentationPage/PresentationComponents/SlidesBar.jsx
@@ -2,27 +2,41 @@ import { Box, IconButton } from "@mui/material";
 import { ArrowRight, ArrowLeft } from '@mui/icons-material';
 import SlidesBarComponent from "./SlidePanelsBar/SlidesBarComponent";
 import AddSlidesBarComponent from "./SlidePanelsBar/AddSlidesBarComponent";
+import { useSelector } from 'react-redux';
+import { getSlides } from "../../../HelperFiles/helper";
 
-const SlidesBar = ({ slides }) => {
-    console.log(slides);
+const SlidesBar = () => {
+  // slides = [
+  //   {
+  //     slideNum: 1
+  //   },
+  //   {
+  //     slideNum: 2
+  //   },
+  //   {
+  //     slideNum: 3
+  //   }
+  // ]
 
-    return (
-        <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", backgroundColor: "primary.main", width: "100%", minHeight: 150 }}>
-          <IconButton sx={{ color: 'white' }}>
-            <ArrowLeft />
-          </IconButton >
-          <Box sx={{display: "flex", alignItems: "center", gap: "2%", width: "100%", height: "100%", overflowX: "auto"}}>
-          {slides?.map((slide) => (
-            <SlidesBarComponent key={slide.slideNum} index={slide.slideNum} />
-          ))}
-            
-            <AddSlidesBarComponent />
-          </Box>
-          <IconButton sx={{ color: 'white' }}>
-            <ArrowRight />
-          </IconButton>
-        </Box>
-    );
+  const presentations = useSelector((state) => state.presentations.presentations);
+
+  return (
+    <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", backgroundColor: "primary.main", width: "100%", minHeight: 150 }}>
+      <IconButton disabled={location.hash.split("/")[1] == 1} sx={{ color: 'white' }}>
+        <ArrowLeft />
+      </IconButton >
+      <Box sx={{display: "flex", alignItems: "center", gap: "2%", width: "100%", height: "100%", overflowX: "auto"}}>
+        {getSlides(presentations)?.map((slide) => (
+          <SlidesBarComponent key={slide.slideNum} index={slide.slideNum} />
+        ))}
+        
+        <AddSlidesBarComponent />
+      </Box>
+      <IconButton disabled={location.hash.split("/")[1] == getSlides(presentations)?.length} sx={{ color: 'white' }}>
+        <ArrowRight />
+      </IconButton>
+    </Box>
+  );
 }
 
 export default SlidesBar;
