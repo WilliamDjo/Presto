26eed6b81a056b3a4606f64d8d59c8429ee4927d
Spandan Shrestha <spandan@Spandans-MacBook-Pro.local>
2024-11-10 00:22:43 +1100

    Implemented delete slides functionality and also added slides settings button

--- frontend/src/Pages/PresentationPage/PresentationComponents/SlidesBar/SlidesBarComponent.jsx
+++ frontend/src/Pages/PresentationPage/PresentationComponents/SlidesBar/SlidesBarComponent.jsx
@@ -2,6 +2,10 @@ import { Box, Typography, IconButton } from '@mui/material';
 import { Settings, Delete } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
+import { useDispatch } from 'react-redux';
+import { deleteSlide } from '../../../../State/presentationsSlice';
+import { getSlides } from '../../../../HelperFiles/helper';
+import { useSelector } from 'react-redux';
 
 const SlidesBarComponent = ({ index, sx = { 
   height: "60%", 
@@ -19,12 +23,19 @@ const SlidesBarComponent = ({ index, sx = {
   },
   border: index == location.hash.split("/")[1] ? "2px solid black" : ""
 }}) => {
+  const presentations = useSelector(state => state.presentations.presentations);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const theme = useTheme();
+
+  // Function to handle box click and navigate
+  const handleBoxClick = () => {
+    navigate(`${location.pathname}#/${index}`);
+  };
 
   return (
     <Box 
-      onClick={() => navigate(`${location.pathname}#/${index}`)} 
+      onClick={handleBoxClick} 
       sx={{ 
         ...sx, 
         position: "relative", 
@@ -46,10 +57,25 @@ const SlidesBarComponent = ({ index, sx = {
           transition: "opacity 0.3s ease" // Smooth transition
         }}
       >
-        <IconButton size="small" sx={{ color: theme.palette.primary.main }}>
+        {/* Prevent propagation for button clicks */}
+        <IconButton size="small" sx={{ color: theme.palette.primary.main }} onClick={(e) => e.stopPropagation()}>
           <Settings />
         </IconButton>
-        <IconButton size="small" sx={{ color: "red" }}>
+        <IconButton 
+          size="small" 
+          sx={{ color: "red" }} 
+          onClick={(e) => { 
+            e.stopPropagation();
+            dispatch(deleteSlide(index));
+            if (index <= parseInt(location.hash.split("/")[1]) && parseInt(location.hash.split("/")[1]) !== 1) {
+              navigate(`${location.pathname}#/${parseInt(location.hash.split("/")[1]) - 1}`);
+            }
+            if (getSlides(presentations).length === 1) {
+              console.log('Prompt user to delete presentation');
+              // TODO: Add modal to prompt user if they want to delete the presentation
+            }
+          }}
+        >
           <Delete />
         </IconButton>
       </Box>
