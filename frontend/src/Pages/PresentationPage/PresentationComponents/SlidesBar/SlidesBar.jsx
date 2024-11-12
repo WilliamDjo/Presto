e2a6e14b2a16a180ae6ffd55f714ab46d83e7ecc
Spandan Shrestha <spandan@Spandans-MacBook-Pro.local>
2024-11-12 17:43:59 +1100

    Implemented rearrangable slides

--- frontend/src/Pages/PresentationPage/PresentationComponents/SlidesBar/SlidesBar.jsx
+++ frontend/src/Pages/PresentationPage/PresentationComponents/SlidesBar/SlidesBar.jsx
@@ -3,45 +3,103 @@ import { ArrowRight, ArrowLeft } from '@mui/icons-material';
 import SlidesBarComponent from "./SlidesBarComponent";
 import AddSlidesBarComponent from "./AddSlidesBarComponent";
 import { useSelector } from 'react-redux';
-import { getSlides } from "../../../../HelperFiles/helper";
+import { getSlidePositionById, getSlides } from "../../../../HelperFiles/helper";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
+import { DndContext, KeyboardSensor, PointerSensor, TouchSensor, closestCenter, useSensor } from "@dnd-kit/core";
+import { arrayMove, horizontalListSortingStrategy, SortableContext, sortableKeyboardCoordinates } from "@dnd-kit/sortable";
+import { useDispatch } from "react-redux";
+import { updateSlidesBarOrder } from "../../../../State/presentationsSlice";
 
 const SlidesBar = () => {
   const presentations = useSelector((state) => state.presentations.presentations);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [dragAutoScrollable, setDragAutoScrollable] = useState(true);
+  const [scrollWidth, setScrollWidth] = useState(null);
+  const slidesContainer = useRef(null);
+  const slidesBar = useRef(null);
 
   useEffect(() => {
     const handleKeyboardInput = (e) => {
       if (e.key === 'ArrowLeft' && parseInt(location.hash.split("/")[1]) > 1) {
-        navigate(`${location.pathname}#/${parseInt(location.hash.split("/")[1]) - 1}`)
+        navigate(`${location.pathname}#/${parseInt(location.hash.split("/")[1]) - 1}`);
       }
       if (e.key === 'ArrowRight' && parseInt(location.hash.split("/")[1]) < getSlides(presentations).length) {
-        navigate(`${location.pathname}#/${parseInt(location.hash.split("/")[1]) + 1}`)
+        navigate(`${location.pathname}#/${parseInt(location.hash.split("/")[1]) + 1}`);
       }
-    }
+    };
 
     document.addEventListener("keydown", handleKeyboardInput);
     return () => document.removeEventListener("keydown", handleKeyboardInput);
   }, [presentations, navigate]);
 
+  const handleDragEnd = (e) => {
+    if (e.active.id === e.over.id) {
+      return;
+    }
+
+    const activeIndex = getSlidePositionById(presentations, e.active.id) - 1;
+    const overIndex = getSlidePositionById(presentations, e.over.id) - 1;
+    
+    dispatch(updateSlidesBarOrder({ active: e.active.id, over: e.over.id }));
+  };
+
+  const handleDragMove = (e) => {
+    const isScrolledToLeft = slidesContainer.current.scrollLeft === 0;
+    const isScrolledToRight = Math.min(slidesContainer.current.scrollLeft, scrollWidth - slidesContainer.current.clientWidth) === scrollWidth - slidesContainer.current.clientWidth;
+    
+    const parentRect = slidesContainer.current.getBoundingClientRect();
+    const itemRect = e.active.rect.current.translated;
+    const parentMiddleX = parentRect.left + parentRect.width / 2;
+    const isDraggedToLeftSide = itemRect.right <= parentMiddleX;
+    const isDraggedToRightSide = itemRect.left >= parentMiddleX;
+
+    const isWithinParent =
+        (itemRect.top >= parentRect.top && itemRect.bottom <= parentRect.bottom) &&
+        ((isDraggedToRightSide && !isScrolledToRight) ||
+        (isDraggedToLeftSide && !isScrolledToLeft));
+
+    setDragAutoScrollable(isWithinParent);
+  };
+
+  const handleDragStart = () => {
+    setScrollWidth(slidesContainer.current.scrollWidth);
+  };
+
+  const sensors = useSensor(
+    useSensor(PointerSensor),
+    useSensor(TouchSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
   return (
-    <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", backgroundColor: "primary.main", width: "100%", minHeight: 150 }}>
-      <IconButton disabled={location.hash.split("/")[1] == 1} onClick={() => {navigate(`${location.pathname}#/${parseInt(location.hash.split("/")[1]) - 1}`)}} sx={{ color: 'white' }}>
+    <Box sensors={sensors} ref={slidesBar} sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", backgroundColor: "primary.main", width: "100%", minHeight: 100 }}>
+      <IconButton disabled={location.hash.split("/")[1] == 1} onClick={() => { navigate(`${location.pathname}#/${parseInt(location.hash.split("/")[1]) - 1}`); }} sx={{ color: 'white' }}>
         <ArrowLeft />
       </IconButton >
-      <Box sx={{display: "flex", alignItems: "center", gap: "2%", width: "100%", height: "100%", overflowX: "auto"}}>
-        {getSlides(presentations)?.map((slide) => (
-          <SlidesBarComponent key={slide.slideNum} index={slide.slideNum} />
-        ))}
-        
+
+      <Box ref={slidesContainer} sx={{ display: "flex", alignItems: "center", gap: "2%", width: "100%", height: "100%", overflowX: "auto" }}>
+        <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd} onDragMove={handleDragMove} autoScroll={dragAutoScrollable} onDragStart={handleDragStart}>
+          {getSlides(presentations) && (
+            <SortableContext items={getSlides(presentations)} strategy={horizontalListSortingStrategy}>
+              {getSlides(presentations)?.map((slide) => (
+                <SlidesBarComponent id={slide.id} key={slide.slideNum} index={slide.slideNum} />
+              ))}
+            </SortableContext>
+          )}
+        </DndContext>
+
         <AddSlidesBarComponent />
       </Box>
-      <IconButton disabled={location.hash.split("/")[1] == getSlides(presentations)?.length} onClick={() => {navigate(`${location.pathname}#/${parseInt(location.hash.split("/")[1]) + 1}`)}} sx={{ color: 'white' }}>
+
+      <IconButton disabled={location.hash.split("/")[1] == getSlides(presentations)?.length} onClick={() => { navigate(`${location.pathname}#/${parseInt(location.hash.split("/")[1]) + 1}`); }} sx={{ color: 'white' }}>
         <ArrowRight />
       </IconButton>
     </Box>
   );
-}
+};
 
 export default SlidesBar;
--- frontend/src/Pages/PresentationPage/PresentationComponents/SlidesBar/SlidesBarComponent.jsx
+++ frontend/src/Pages/PresentationPage/PresentationComponents/SlidesBar/SlidesBarComponent.jsx
@@ -1,88 +1,156 @@
-import { Box, Typography, IconButton } from '@mui/material';
+import React, { useEffect, useState, useRef } from 'react';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+import { Box, IconButton } from '@mui/material';
 import { Settings, Delete } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { deleteSlide } from '../../../../State/presentationsSlice';
-import { getSlides } from '../../../../HelperFiles/helper';
-import { useSelector } from 'react-redux';
+import { getSlides, getSlideByPosition } from '../../../../HelperFiles/helper';
+import Block from '../../SlideDisplay/SlideDisplayComponents/Block';
 
-const SlidesBarComponent = ({ index, sx = { 
-  height: "60%", 
-  minWidth: "8%", 
-  backgroundColor: "white", 
-  display: "flex", 
-  justifyContent: "center", 
-  alignItems: "center", 
-  borderRadius: "10px", 
-  cursor: "pointer", 
-  boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
-  '&:hover': {
-    backgroundColor: "#f0f0f0",
-    boxShadow: "0px 6px 12px rgba(0, 0, 0, 0.15)",
-  },
-  border: index == location.hash.split("/")[1] ? "2px solid black" : ""
-}}) => {
+const SlidesBarComponent = ({ id, index }) => {
+  const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({animateLayoutChanges: () => false , id });
+  const [isDraggingEvent, setIsDraggingEvent] = useState(false);
+  const [mousePosition, setMousePosition] = useState(null);
+  const [parentDimensions, setParentDimensions] = useState({ width: 0, height: 0 });
   const presentations = useSelector(state => state.presentations.presentations);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const theme = useTheme();
+  const parentRef = useRef(null);
 
-  // Function to handle box click and navigate
-  const handleBoxClick = () => {
-    navigate(`${location.pathname}#/${index}`);
+  // Retrieve the parent container's dimensions on mount
+  useEffect(() => {
+    const updateDimensions = () => {
+      const { width, height } = parentRef.current.getBoundingClientRect();
+      setParentDimensions({ width, height });
+    };
+
+    updateDimensions();
+
+    window.addEventListener("resize", () => updateDimensions());
+    return () => window.removeEventListener("resize", () => updateDimensions());
+  }, []);
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+    cursor: isDraggingEvent ? 'grabbing' : 'pointer',
+    position: 'relative',
+    minWidth: "8%",
+    backgroundColor: "white", 
+    display: "flex", 
+    justifyContent: "center", 
+    alignItems: "center", 
+    borderRadius: "10px", 
+    boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+    overflow: "hidden",
+    '&:hover': {
+      backgroundColor: "#f0f0f0",
+      boxShadow: "0px 6px 12px rgba(0, 0, 0, 0.15)",
+    },
+    border: index === parseInt(location.hash.split("/")[1]) ? "2px solid black" : ""
+  };
+
+  useEffect(() => {
+    const handleMouseMove = () => setIsDraggingEvent(true);
+
+    if (isDragging) {
+      window.addEventListener("mousemove", handleMouseMove);
+      setIsDraggingEvent(false);
+    } else {
+      window.removeEventListener("mousemove", handleMouseMove);
+      setIsDraggingEvent(false);
+    }
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [isDragging]);
+
+  const handleMouseDown = (e) => {
+    setMousePosition({ x: e.clientX, y: e.clientY });
+  };
+
+  const handleMouseUp = (e) => {
+    if (Math.pow(mousePosition.x - e.clientX, 2) + Math.pow(mousePosition.y - e.clientY, 2) <= 100) {
+      if (e.target.closest('#delete-button')) {
+        handleDeleteClick();
+      } else if (e.target.closest('#settings-button')) {
+        console.log('Settings button clicked');
+      } else {
+        navigate(`${location.pathname}#/${index}`);
+      }
+    } else {
+      console.log('Drag event detected');
+    }
+  };
+
+  const handleDeleteClick = () => {
+    dispatch(deleteSlide(index));
+    if (index <= parseInt(location.hash.split("/")[1]) && parseInt(location.hash.split("/")[1]) !== 1) {
+      navigate(`${location.pathname}#/${parseInt(location.hash.split("/")[1]) - 1}`);
+    }
+    if (getSlides(presentations).length === 1) {
+      console.log('Prompt user to delete presentation');
+    }
   };
 
   return (
-    <Box 
-      onClick={handleBoxClick} 
-      sx={{ 
-        ...sx, 
-        position: "relative", 
-        '&:hover .icon-buttons': { // Show buttons on hover
-          opacity: 1 
-        }
+    // This is the parent box
+    <Box
+      ref={(el) => {
+        setNodeRef(el);
+        parentRef.current = el;
+      }}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      {...attributes}
+      {...listeners}
+      sx={{
+        ...style,
+        'touchAction': 'none',
+        'aspectRatio': 16/9,
+        '&:hover .icon-buttons': { opacity: 1 }
       }}
     >
       <Box 
         className="icon-buttons" 
         sx={{ 
-          position: "absolute", 
+          position: "absolute",
           top: 0, 
           right: 0, 
           display: "flex", 
           justifyContent: "space-between", 
           width: "100%", 
-          opacity: 0, // Initially hidden
-          transition: "opacity 0.3s ease" // Smooth transition
+          opacity: 0, 
+          transition: "opacity 0.3s ease"
         }}
       >
-        {/* Prevent propagation for button clicks */}
-        <IconButton size="small" sx={{ color: theme.palette.primary.main }} onClick={(e) => e.stopPropagation()}>
+        <IconButton id="settings-button" size="small" sx={{ color: theme.palette.primary.main }} onClick={(e) => e.stopPropagation()}>
           <Settings />
         </IconButton>
-        <IconButton 
+        <IconButton
+          id="delete-button" 
           size="small" 
-          sx={{ color: "red" }} 
-          onClick={(e) => { 
-            e.stopPropagation();
-            dispatch(deleteSlide(index));
-            if (index <= parseInt(location.hash.split("/")[1]) && parseInt(location.hash.split("/")[1]) !== 1) {
-              navigate(`${location.pathname}#/${parseInt(location.hash.split("/")[1]) - 1}`);
-            }
-            if (getSlides(presentations).length === 1) {
-              console.log('Prompt user to delete presentation');
-              // TODO: Add modal to prompt user if they want to delete the presentation
-            }
-            // TODO: Add modal to confirm if the user really wants to delete the slide
-          }}
+          sx={{ color: "red" }}
         >
           <Delete />
         </IconButton>
       </Box>
-      <Typography>
-        {index}
-      </Typography>
+      
+      {getSlideByPosition(presentations, index)?.contents.map((element) => (
+        <Block
+          interactable={false} 
+          parentHeight={parentDimensions.height} 
+          parentWidth={parentDimensions.width} 
+          key={element.index} 
+          index={element.index} 
+          slideNum={index}
+        />
+      ))}
     </Box>
   );
 };
