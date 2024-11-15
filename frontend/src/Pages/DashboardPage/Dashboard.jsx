dd15069cb383ad704443d11ce68feeac5a1112c9
Spandan Shrestha <spandan@Spandans-MacBook-Pro.local>
2024-11-15 13:44:11 +1100

    Fix dashboard layout

--- frontend/src/Pages/DashboardPage/Dashboard.jsx
+++ frontend/src/Pages/DashboardPage/Dashboard.jsx
@@ -2,8 +2,7 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import CustomButton from "../../Components/CustomButton";
 import { logoutFetch } from "../../HelperFiles/helper";
-import Grid from '@mui/material/Grid2';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
+import Grid from '@mui/material/Grid';
 import {
   Dialog,
   DialogTitle,
@@ -11,13 +10,15 @@ import {
   DialogActions,
   TextField,
   Card,
-  CardContent,
   Typography,
   Box,
   Container,
   Alert,
-  CssBaseline
+  CssBaseline,
+  CardMedia,
+  IconButton,
 } from '@mui/material';
+import { Image, AddCircle, Logout } from '@mui/icons-material';
 import { useDispatch, useSelector } from 'react-redux';
 import { setPresentations, savePresentations, createNewPresentation } from '../../State/presentationsSlice';
 import ThumbnailUpload from '../../Components/ThumbnailUpload';
@@ -65,10 +66,10 @@ const DashboardPage = () => {
     setError('');
 
     if (presentationTitle.trim()) {
-      dispatch(createNewPresentation({presentationTitle, presentationDescription, presentationThumbnail}));
+      dispatch(createNewPresentation({ presentationTitle, presentationDescription, presentationThumbnail }));
       setIsModalOpen(false);
     } else {
-      setError('Please enter valid a name');
+      setError('Please enter a valid name');
     }
   };
 
@@ -97,117 +98,138 @@ const DashboardPage = () => {
     <>
       <CssBaseline />
       <Container maxWidth="lg" tabIndex={-1}>
-        <Box sx={{ py: 4 }}>
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              mb: 4
-            }}
-          >
-            <Typography variant="h4" component="h1">
-              Presentations
-            </Typography>
-            <CustomButton
-              text="New Presentation"
-              variant="contained"
-              color="primary"
-              startIcon={<AddCircleIcon />}
-              onClick={() => { setIsModalOpen(true); setError('') }}
-            />
-          </Box>
+        <Box sx={{ display: 'flex', alignItems: 'center', py: 2 }}>
+          <IconButton onClick={handleLogout} color="primary" sx={{ mr: 2 }}>
+            <Logout fontSize="large" sx={{ transform: 'scaleX(-1)' }} />
+          </IconButton>
+          <Typography variant="h4" component="h1" sx={{ flexGrow: 1 }}>
+            Presentations
+          </Typography>
+          <CustomButton
+            text="New Presentation"
+            variant="contained"
+            color="primary"
+            startIcon={<AddCircle />}
+            onClick={() => { setIsModalOpen(true); setError('') }}
+            fullWidth={false}
+          />
+        </Box>
 
-          <Dialog
-            open={isModalOpen}
-            onClose={() => setIsModalOpen(false)}
-            maxWidth="sm"
-            fullWidth
-            aria-hidden={!isModalOpen}
-          >
-            <DialogTitle>Create New Presentation</DialogTitle>
-            {error && <Alert severity="error" sx={{ mb: 2 }}>{error}</Alert>}
-            <Box component="form" onSubmit={handleCreateNewPresentation}>
-              <DialogContent>
-                <TextField
-                  autoFocus
-                  margin="dense"
-                  label="Presentation Name"
-                  type="text"
-                  fullWidth
-                  variant="outlined"
-                  value={presentationTitle}
-                  onChange={(e) => setPresentationTitle(e.target.value)}
-                  sx={{ mt: 2 }}
-                />
-                <TextField
-                  autoFocus
-                  margin="dense"
-                  label="Presentation Description"
-                  type="text"
-                  fullWidth
-                  variant="filled" // or "standard"
-                  multiline
-                  minRows={4} // Adjust the number of rows as needed
-                  value={presentationDescription}
-                  onChange={(e) => setPresentationDescription(e.target.value)}
-                  sx={{ mt: 2 }}
-                />
+        <Dialog
+          open={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          maxWidth="sm"
+          fullWidth
+          aria-hidden={!isModalOpen}
+        >
+          <DialogTitle>Create New Presentation</DialogTitle>
+          {error && <Alert severity="error" sx={{ mb: 2 }}>{error}</Alert>}
+          <Box component="form" onSubmit={handleCreateNewPresentation}>
+            <DialogContent>
+              <TextField
+                autoFocus
+                margin="dense"
+                label="Presentation Name"
+                type="text"
+                fullWidth
+                variant="outlined"
+                value={presentationTitle}
+                onChange={(e) => setPresentationTitle(e.target.value)}
+                sx={{ mt: 2 }}
+              />
+              <TextField
+                margin="dense"
+                label="Presentation Description"
+                type="text"
+                fullWidth
+                variant="filled"
+                multiline
+                minRows={4}
+                value={presentationDescription}
+                onChange={(e) => setPresentationDescription(e.target.value)}
+                sx={{ mt: 2 }}
+              />
 
-                <ThumbnailUpload 
-                  thumbnail={presentationThumbnail}
-                  resetThumbnail={() => setPresentationThumbnail("")}
-                  onThumbnailChange={handleThumbnailChange}
-                />
-
-              </DialogContent>
-              <DialogActions sx={{ px: 3, pb: 3 }}>
-                <CustomButton
-                  onClick={handleCreateNewPresentation}
-                  variant="contained"
-                  disabled={!presentationTitle.trim()}
-                  text="Create"
-                />
-              </DialogActions>
-            </Box>
+              <ThumbnailUpload
+                thumbnail={presentationThumbnail}
+                resetThumbnail={() => setPresentationThumbnail("")}
+                onThumbnailChange={handleThumbnailChange}
+              />
+            </DialogContent>
+            <DialogActions sx={{ px: 3, pb: 3 }}>
+              <CustomButton
+                onClick={handleCreateNewPresentation}
+                variant="contained"
+                disabled={!presentationTitle.trim()}
+                text="Create"
+              />
+            </DialogActions>
             <DialogActions sx={{ px: 3, pb: 3 }}>
               <CustomButton variant="outlined" text="Cancel" onClick={() => { setIsModalOpen(false); }} />
             </DialogActions>
-          </Dialog>
+          </Box>
+        </Dialog>
 
-          <Grid container spacing={3}>
-            {presentations?.map((presentation) => (
-              <Grid xs={12} sm={6} md={4} key={presentation.id} sx={{ cursor: 'pointer' }} onClick={() => {navigate(`/presentation/${presentation.id}#/1`)}}>
-                <Card
-                  sx={{
-                    height: '100%',
-                    transition: 'all 0.2s',
-                    '&:hover': {
-                      transform: 'translateY(-4px)',
-                      boxShadow: 3
-                    }
-                  }}
-                >
-                  <CardContent>
-                    <Typography variant="h6" component="h2" gutterBottom>
-                      {presentation.title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {presentation.slides.length} slide{presentation.slides.length !== 1 ? 's' : ''}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
-        <CustomButton text="Log out" onClick={handleLogout} />
- 
-    
+        <Grid container spacing={3}>
+          {presentations?.map((presentation) => (
+            <Grid item xs={12} sm={6} md={4} key={presentation.id}>
+              <Card
+                sx={{
+                  width: '100%',
+                  minWidth: '100px',
+                  aspectRatio: '2 / 1', // Set 2:1 ratio
+                  display: 'flex',
+                  alignItems: 'center',
+                  p: 2,
+                  cursor: 'pointer',
+                  transition: 'all 0.2s',
+                  '&:hover': {
+                    transform: 'translateY(-4px)',
+                    boxShadow: 3
+                  },
+                }}
+                onClick={() => navigate(`/presentation/${presentation.id}#/1`)}
+              >
+                {presentation.thumbnail ? (
+                  <CardMedia
+                    component="img"
+                    image={presentation.thumbnail}
+                    alt={presentation.title}
+                    sx={{
+                      width: 64, // Display as an icon-sized image
+                      height: 64,
+                      borderRadius: '4px',
+                      mr: 2
+                    }}
+                  />
+                ) : (
+                  <Image
+                    sx={{
+                      width: 64,
+                      height: 64,
+                      color: 'grey.500',
+                      mr: 2,
+                    }}
+                  />
+                )}
+                <Box>
+                  <Typography variant="h6" component="h2" noWrap>
+                    {presentation.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary" noWrap>
+                    {presentation.description || 'No description available'}
+                  </Typography>
+                  <Typography variant="caption" color="text.secondary">
+                    {presentation.slides.length} slide{presentation.slides.length !== 1 ? 's' : ''}
+                  </Typography>
+                </Box>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </Container>
-
     </>
-  )
-}
+  );
+};
 
-export default DashboardPage
+export default DashboardPage;
