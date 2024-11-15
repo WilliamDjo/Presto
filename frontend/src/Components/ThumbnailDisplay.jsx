6d3081b9501af5b76d9ffe5a306d20c85e90c373
Spandan Shrestha <spandan@Spandans-MacBook-Pro.local>
2024-11-15 18:27:56 +1100

    Merging to master

---
+++ frontend/src/Components/ThumbnailDisplay.jsx
@@ -0,0 +1,23 @@
+import { Box } from '@mui/material';
+import { Image } from '@mui/icons-material';
+
+const ThumbnailDisplay = ({ thumbnail, size = 40 }) => {
+  if (!thumbnail) { // Check for both null/undefined and default value
+    return <Image sx={{ width: size, height: size, color: 'white' }} />;
+  }
+
+  return (
+    <Box
+      component="img"
+      src={thumbnail}
+      sx={{
+        width: size,
+        height: size,
+        objectFit: 'cover',
+        borderRadius: 1
+      }}
+    />
+  );
+};
+
+export default ThumbnailDisplay;
